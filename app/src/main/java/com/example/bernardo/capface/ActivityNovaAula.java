e914f13db0a407368bc64589660ebc8de511d843
Bernardo Biesseck <biesseck@hotmail.com>
2018-11-21 02:49:16 -0300

    Valida o formulario e implementa algumas melhorias de interface com o usuario

--- app/src/main/java/com/example/bernardo/capface/ActivityNovaAula.java
+++ app/src/main/java/com/example/bernardo/capface/ActivityNovaAula.java
@@ -1,5 +1,7 @@
 package com.example.bernardo.capface;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,9 +14,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.bernardo.capface.entidades.ControllerCapturedImages;
@@ -24,15 +28,17 @@ import com.example.bernardo.capface.entidades.Disciplina;
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ActivityNovaAula extends AppCompatActivity {
 
     int REQUEST_IMAGE_CAPTURE = 1;
 
-    EditText editTextCurso;
+    Spinner spinnerCursos;
     Spinner spinnerDisciplina;
     Spinner spinnerTipoDisciplina;
     EditText editTextDataAula;
@@ -50,7 +56,6 @@ public class ActivityNovaAula extends AppCompatActivity {
     String diretorioPadraoCapfaceAulas = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString()+ "/CapFace/Aulas";
     String diretorioAulaAtual, pathDiretorioAulaAtual;
     private long dataHoraInicial;
-    private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     ArrayList<String> arrayListNomesImagensCapturadas = new ArrayList<>();
 
 
@@ -71,13 +76,44 @@ public class ActivityNovaAula extends AppCompatActivity {
 
 
     public void inicializarComponentes() {
-        editTextCurso = (EditText) findViewById(R.id.editTextCurso);
+        spinnerCursos = (Spinner) findViewById(R.id.spinnerCursos);
         spinnerDisciplina = (Spinner) findViewById(R.id.spinnerDisciplina);
         spinnerTipoDisciplina = (Spinner) findViewById(R.id.spinnerTipoDisciplina);
+
         editTextDataAula = (EditText) findViewById(R.id.editTextDataAula);
+        editTextDataAula.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(MotionEvent.ACTION_UP == event.getAction()) {
+                    aoClicarParaSelecionarDataDaAula();
+                }
+                return false;
+            }
+        });
+
         editTextQtdeAulas = (EditText) findViewById(R.id.editTextQtdeAulas);
+
         editTextHoraInicioAula = (EditText) findViewById(R.id.editTextHoraInicioAula);
+        editTextHoraInicioAula.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(MotionEvent.ACTION_UP == event.getAction()) {
+                    aoClicarParaSelecionarHoraDaAula(editTextHoraInicioAula);
+                }
+                return false;
+            }
+        });
+
         editTextHoraFimAula = (EditText) findViewById(R.id.editTextHoraFimAula);
+        editTextHoraFimAula.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(MotionEvent.ACTION_UP == event.getAction()) {
+                    aoClicarParaSelecionarHoraDaAula(editTextHoraFimAula);
+                }
+                return false;
+            }
+        });
 
         buttonCamera = (Button) findViewById(R.id.buttonCamera);
         buttonCamera.setOnClickListener(new View.OnClickListener() {
@@ -121,24 +157,41 @@ public class ActivityNovaAula extends AppCompatActivity {
 
 
     public String getDirNameAulaAtualFromFormulario() {
-        String curso = "TecInformatica";
-        String disciplina = "ProgII";
-        String tipoDisciplina = "Bimestral";
-        String dataAula = "20-11-2018";
-        String qtdeAulas = "2aulas";
-        String horaInicio = "07-00";
-        String horaFim = "08h40";
+        String curso = (String) spinnerCursos.getSelectedItem();
+        String disciplina = ((Disciplina) spinnerDisciplina.getSelectedItem()).getNome();
+        String tipoDisciplina = (String) spinnerTipoDisciplina.getSelectedItem();
+        String dataAula = editTextDataAula.getText().toString().replace('/', '-');
+        String qtdeAulas = editTextQtdeAulas.getText().toString();
+        String horaInicio = editTextHoraInicioAula.getText().toString().replace(':', '-');
+        String horaFim = editTextHoraFimAula.getText().toString().replace(':', '-');
 
         String dirNameAulaAtual = curso + "_" + disciplina + "_" + tipoDisciplina + "_" + dataAula + "_" + qtdeAulas + "_" + horaInicio + "_" + horaFim;
         return dirNameAulaAtual;
     }
 
 
+    public boolean formularioAulaEstaValido() {
+        if (!editTextDataAula.getText().toString().equals("") &&
+            !editTextQtdeAulas.getText().toString().equals("") &&
+            !editTextHoraInicioAula.getText().toString().equals("") &&
+            !editTextHoraFimAula.getText().toString().equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     public void aoClicarNoBotaoCamera() {
-        //Intent irParaCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);            // SALVA UMA UNICA FOTO QUANDO FECHA A CAMERA
-        Intent irParaCamera = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);  // PERMITE SALVAR VARIAS IMAGENS ANTES DE FECHAR A CAMERA
-        dataHoraInicial = new Date().getTime(); // Capturar data e hora do sistema (dataHoraInicial)
-        startActivityForResult(irParaCamera, REQUEST_IMAGE_CAPTURE);    // inicializa a camera
+        if (formularioAulaEstaValido()) {
+            //Intent irParaCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);            // SALVA UMA UNICA FOTO QUANDO FECHA A CAMERA
+            Intent irParaCamera = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);  // PERMITE SALVAR VARIAS IMAGENS ANTES DE FECHAR A CAMERA
+            dataHoraInicial = new Date().getTime(); // Capturar data e hora do sistema (dataHoraInicial)
+            startActivityForResult(irParaCamera, REQUEST_IMAGE_CAPTURE);    // inicializa a camera
+
+        } else {
+            exibirToastNotification("Preencha os dados da aula!", Toast.LENGTH_LONG);
+        }
     }
 
 
@@ -164,16 +217,61 @@ public class ActivityNovaAula extends AppCompatActivity {
             this.atualizarListViewImageFilesAulaAtual(arrayListNomesImagensCapturadas);
             buttonEnviar.setEnabled(true);
 
+            /*
             for (int i = 0; i < arrayListNomesImagensCapturadas.size(); i++) {
                 Log.i("ActivityNovaAula", "onActivityResult() -> FOTO_TIRADA_AGORA - FileName:" + arrayListNomesImagensCapturadas.get(i));
             }
             Log.i("ActivityNovaAula", "onActivityResult() -> Qtde de imagens capturadas agora: " + arrayListNomesImagensCapturadas.size());
+            */
 
         } else {
             buttonEnviar.setEnabled(false);
-            Log.i("ActivityNovaAula", "Nenhuma imagem capturada para esta aula");
+            //Log.i("ActivityNovaAula", "Nenhuma imagem capturada para esta aula");
             exibirToastNotification("Nenhuma imagem capturada!", Toast.LENGTH_LONG);
         }
+    }
+
+
+    public void aoClicarParaSelecionarDataDaAula() {
+        Calendar mCurrentDate = Calendar.getInstance();
+        int dia = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+        int mes = mCurrentDate.get(Calendar.MONTH);
+        int ano = mCurrentDate.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    String dataString = dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+                    Date dataDate = sdf.parse(dataString);
+                    editTextDataAula.setText(sdf.format(dataDate));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ano, mes, dia);
+        datePickerDialog.show();
+    }
+
+
+    private void aoClicarParaSelecionarHoraDaAula(final EditText editText) {
+        TimePickerDialog.OnTimeSetListener  mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hora, int minuto) {
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    String horarioString = hora + ":" + minuto;
+                    Date horarioDate = sdf.parse(horarioString);
+                    editText.setText(sdf.format(horarioDate));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Calendar calNow = Calendar.getInstance();
+        TimePickerDialog dialog = new TimePickerDialog(this, mTimeSetListener, calNow.get(Calendar.HOUR_OF_DAY), calNow.get(Calendar.MINUTE), true);
+        dialog.show();
     }
 
 
